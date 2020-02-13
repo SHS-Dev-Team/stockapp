5a92d91b9b4ca3e182fb2eb6f5cfce0a1d69228f
ksap1 <kylesaltandpepper@gmail.com>
2020-02-12 22:15:15 -0500

    please don't open the folder stockapp yet. It's unfinished for now.

--- stockapp/blog/views.py
+++ stockapp/blog/views.py
@@ -5,11 +5,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 def createPost(request):
     return HttpResponse("Use Template View")
 
-    
+
 def index(request):
     return HttpResponse("All stakes in now")
 
 def blogHome(request):
     return HttpResponse("Have to edit this")
  
-# Create your views here.
+

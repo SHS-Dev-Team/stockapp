ae54005cf57d49ad4c77cb417f21f7f4d950dfef
ksap1 <kylesaltandpepper@gmail.com>
2020-02-15 16:03:48 -0500

    not finished yet

--- stockapp/blog/urls.py
+++ stockapp/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('home/',views.index, name='home'),
-    path('home/new',views.index, name ='home'),
+    path('home/new',TemplateView.as_view(template_name='input.html'), name ='newpost'),
 ]
--- stockapp/blog/views.py
+++ stockapp/blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-
+from datetime import datetime as dt
 
 def createPost(request):
-    return HttpResponse("Use Template View")
-
+    newPost = Post(title=request.POST['title'], text=request.POST['text'], date=dt.now())
+    newPost.save()
+    return HttpResponse("saved")
 
 def index(request):
     return HttpResponse("All stakes in now")

001cd5b91c4ecee74d167ab553bcafc5a7a1f84a
ksap1 <kylesaltandpepper@gmail.com>
2020-02-16 13:05:25 -0500

    update site

--- stockapp/blog/urls.py
+++ stockapp/blog/urls.py
@@ -5,5 +5,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('home/',views.index, name='home'),
     path('home/new',TemplateView.as_view(template_name='input.html'), name ='newpost'),
-    path('saved/',views.createPost,name="createPost")
+    path('saved/',views.createPost,name="createPost"),
+    path('delete/',views.listDel),
+    path('delete/<str:id>/',views.delete,name="delete")
 ]
--- stockapp/blog/views.py
+++ stockapp/blog/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from datetime import datetime as dt
 from .models import *
+import uuid
 
 def createPost(request):
     newPost = Post(title=request.POST['title'], body=request.POST['body'], date=dt.now())
     newPost.save()
-    return HttpResponse("saved")
+    return index(request)
 
 def index(request):
     posts = {"posts":Post.objects.all()}
@@ -15,4 +17,14 @@ def index(request):
 def blogHome(request):
     return HttpResponse("Have to edit this")
  
+def listDel(request):
+    posts = {"posts":Post.objects.all()}
+    return render(request,'blog/delete.html',posts)
+
+
+def delete(request,id):
+    prod = Post.objects.get(title=id)
+    prod.delete()
+    return HttpResponseRedirect(reverse('home'))
+
 

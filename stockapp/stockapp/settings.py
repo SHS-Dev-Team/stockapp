53e9bb4ea8dfa786e540447e85be9ac29bac29e6
ksap1 <kylesaltandpepper@gmail.com>
2021-01-14 16:16:38 -0500

    changed database to sqlite3

--- stockapp/stockapp/settings.py
+++ stockapp/stockapp/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'stockapp.wsgi.application'
 
 # [START dbconfig]
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -88,7 +88,13 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
 
 
 # In the flexible environment, you connect to CloudSQL using a unix socket.
